6f519b7da3dec6c263716fbbba1e8aeb413298fc
paulobeckman <beckmam.paulo@hotmail.com>
2021-12-16 11:38:10 -0300

    feat: authentication with SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
+import { GetServerSideProps } from "next";
 import { FormEvent, useContext, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import { parseCookies } from "nookies";
+import { withSSRGuest } from "../utils/withSSRGuest";
 
 export default function Home() {
   const [email, setEmail] = useState('');
@@ -26,3 +29,9 @@ export default function Home() {
     </form>
   )
 }
+
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return {
+    props: {}
+  }
+});
